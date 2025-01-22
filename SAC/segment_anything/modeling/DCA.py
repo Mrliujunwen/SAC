84be7e2ddb7e8dbfd0180e849685d88abd33ee76
给大佬倒茶 <3275233597@qq.com>
2025-01-23 00:01:14 +0800

    u

--- SAC/segment_anything/modeling/DCA.py
+++ SAC/segment_anything/modeling/DCA.py
@@ -61,9 +61,9 @@ class LayerNorm(nn.Module):
         return to_4d(self.body(to_3d(x)), h, w)
 
 
-class MDAF(nn.Module):
+class DCA(nn.Module):
     def __init__(self, dim, num_heads, LayerNorm_type, ):
-        super(MDAF, self).__init__()
+        super(DCA, self).__init__()
         self.num_heads = num_heads
 
         self.norm1 = LayerNorm(dim, LayerNorm_type)
